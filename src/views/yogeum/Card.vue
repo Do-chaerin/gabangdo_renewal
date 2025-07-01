9ea2cc55afa1d7e75e917c7b5859da855e85488c
swin3321@gmail.com <swin3321@gmail.com>
2025-07-01 23:07:19 +0900

    Yogeum ~ing

--- src/views/yogeum/Card.vue
+++ src/views/yogeum/Card.vue
@@ -4,92 +4,91 @@
   <div class="cardtotal">
     <!-- 메인 컨텐츠1 전체영역 -->
     <div class="card_wrap">
-      <!-- s사이즈 -->
+      <!-- S사이즈 -->
       <div class="card">
-        <!-- 사이즈 섹션 상단 설명 -->
         <div class="cardtop">
           <h2>S 사이즈</h2>
           <h4>기내용 캐리어,소형 배낭 등</h4>
           <h5>21인치이하/55cm이하/10kg미만</h5>
         </div>
         <img src="/images/gh/ma_content1/s_size.png" />
-        <!-- 당일예약시 -->
-        <div class="carbottum1">
-          <h4>당일예약</h4>
+        <div class="cardbottum1">
+          <h4>당일예약시</h4>
+          &nbsp;
           <h2>12,000원</h2>
+          &nbsp;
         </div>
-        <!-- 사전예약시 -->
         <div class="cardbottum2">
-          <h5>사전예약시 10,000원</h5>
+          <h4>사전예약시</h4>
+          &nbsp;
+          <h2>10,000원</h2>
+          &nbsp;
         </div>
       </div>
-      <!-- m사이즈 -->
+      <!-- M사이즈 -->
       <div class="card">
-        <!-- 사이즈 섹션 상단 설명 -->
         <div class="cardtop">
           <h2>M 사이즈</h2>
           <h4>화물용 캐리어, 등산 가방 등</h4>
           <h5>25인치이하/65cm이하/20kg미만</h5>
         </div>
-        <!-- 아이콘 -->
         <img src="/images/gh/ma_content1/m_size.png" />
-        <!-- 당일예약시 -->
         <div class="cardbottum1">
-          <h4>당일예약</h4>
+          <h4>당일예약시</h4>
+          &nbsp;
           <h2>16,000원</h2>
+          &nbsp;
         </div>
-        <!-- 사전예약시 -->
         <div class="cardbottum2">
-          <h5>사전예약시 14,000원</h5>
+          <h4>사전예약시</h4>
+          &nbsp;
+          <h2>14,000원</h2>
+          &nbsp;
         </div>
       </div>
-      <!-- l사이즈 -->
+      <!-- L사이즈 -->
       <div class="card">
-        <!-- 사이즈 섹션 상단 설명 -->
         <div class="cardtop">
           <h2>L 사이즈</h2>
           <h4>대형 캐리어, 배낭, 골프백 등</h4>
           <h5>26인치이상/66cm이상/30kg미만</h5>
         </div>
         <img src="/images/gh/ma_content1/l-size.png" />
-        <!-- 당일예약시 -->
         <div class="cardbottum1">
-          <h4>당일예약</h4>
+          <h4>당일예약시</h4>
+          &nbsp;
           <h2>18,000원</h2>
         </div>
-        <!-- 사전예약시 -->
         <div class="cardbottum2">
-          <h5>사전예약시 16,000원</h5>
+          <h4>사전예약시</h4>
+          &nbsp;
+          <h2>16,000원</h2>
         </div>
       </div>
       <!-- ?사이즈 -->
       <div class="card">
-        <!-- 사이즈 섹션 상단 설명 -->
         <div class="cardtop">
           <h2>? 사이즈</h2>
           <h4>기타 물품 및 표시 외 사이즈</h4>
           <h5>가방도 문의 후 협의된 물품에 한함</h5>
         </div>
         <img src="/images/gh/ma_content1/size.png" />
-        <!-- 카카오톡 문의 -->
         <div class="cardbottum3">
-          <h2>카카오톡 문의</h2>
+          <h4>협의 후 가능</h4>
+          <h2>가방도 문의</h2>
         </div>
       </div>
     </div>
-    <div class="infoWrapper">
+    <div class="infowrapper">
       <!-- 알림 왼쪽(할인) -->
-      <div class="leftInfo">
-        <p>2개 이상 예약시부터 1,000원씩 추가할인 무한적용</p>
+      <div class="leftinfo">
+        <p>•&nbsp;2개 이상 예약시부터 1,000원씩 추가할인 무한적용</p>
         <p>(3개 예약시 2,000원 / 4개 예약시 3,000원 할인)</p>
       </div>
       <!-- 알림 오른쪽(주의사항) -->
-      <div class="rightInfocaution">
+      <div class="rightinfo">
         <a href="#">추가요금 발생지역 > </a>
-        <p>
-          <img src="/images/geen/caution_icon.png" alt="주의표시" />수하물
-          개당요금
-        </p>
+        <p><img src="/images/geen/caution_icon.png" />수하물 개당요금</p>
       </div>
     </div>
   </div>
@@ -113,10 +112,17 @@
   border: $border-line;
   border-radius: $radius;
   width: 215px;
-  padding: 15px;
+  padding: 10px;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  img {
+    width: 75%;
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    padding: 20px 0;
+  }
 }
 .cardtop {
   display: flex;
@@ -129,9 +135,72 @@
   }
   h4 {
     font-size: 16px;
+    color: #333333;
   }
   h5 {
-    font-size: 13px;
+    font-size: 14px;
+    color: #666666;
+  }
+}
+.cardbottum1 {
+  display: flex;
+  justify-content: flex-end;
+  h4 {
+    font-size: 18px;
+    color: #555555;
+  }
+  h2 {
+    font-size: 18px;
+    color: #ff0000;
+    font-weight: bold;
+  }
+}
+.cardbottum2 {
+  display: flex;
+  justify-content: flex-end;
+  h4 {
+    font-size: 16px;
+    color: #555555;
+  }
+  h2 {
+    font-size: 16px;
+    color: #0000ff;
+    font-weight: bold;
+  }
+}
+.cardbottum3 {
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  h4 {
+    font-size: 18px;
+    color: #ff0000;
+    font-weight: bold;
+  }
+  h2 {
+    font-size: 16px;
+    color: #0000ff;
+    font-weight: bold;
+  }
+}
+.infowrapper {
+  max-width: 1000px;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding-top: 10px;
+  .leftinfo {
+    color: #666666;
+  }
+  .rightinfo {
+    p {
+      display: flex;
+      color: #ff0000;
+    }
+    img {
+      width: 15%;
+    }
   }
 }
 </style>
